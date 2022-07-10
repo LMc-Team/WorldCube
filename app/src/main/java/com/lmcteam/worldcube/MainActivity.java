7045f1d06a58f45f8d7d0860f6112f562d68e7f2
LMcTeam <47357428+LMc-Team@users.noreply.github.com>
2022-07-10 15:00:25 +0800

    Update MainActivity.java

--- app/src/main/java/com/lmcteam/worldcube/MainActivity.java
+++ app/src/main/java/com/lmcteam/worldcube/MainActivity.java
@@ -3,6 +3,7 @@ package com.lmcteam.worldcube;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+Toast.makeText(this,"Fuck",1).show();
     }
 }
