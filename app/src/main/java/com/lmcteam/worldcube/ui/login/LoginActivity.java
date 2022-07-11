767e4081e12e2c2d0d9cb9c6c414c6bf60f9fff3
ZuoWu-TD <gsjtceoxl@163.com>
2022-07-11 08:25:01 +0800

    import res

--- app/src/main/java/com/lmcteam/worldcube/ui/login/LoginActivity.java
+++ app/src/main/java/com/lmcteam/worldcube/ui/login/LoginActivity.java
@@ -2,16 +2,10 @@ package com.lmcteam.worldcube.ui.login;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.annotation.StringRes;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.KeyEvent;
-import android.view.View;
-import android.view.inputmethod.EditorInfo;
-import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
@@ -29,9 +23,9 @@ public class LoginActivity extends AppCompatActivity {
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        final TextInputEditText usernameEditText = (TextInputEditText) binding.username;
-        final TextInputEditText passwordEditText = (TextInputEditText) binding.password;
-        final MaterialButton loginButton = (MaterialButton) binding.login;
+        final TextInputEditText usernameEditText = binding.username;
+        final TextInputEditText passwordEditText = binding.password;
+        final MaterialButton loginButton = binding.login;
 
         TextWatcher afterTextChangedListener = new TextWatcher() {
             @Override
@@ -52,13 +46,10 @@ public class LoginActivity extends AppCompatActivity {
         usernameEditText.addTextChangedListener(afterTextChangedListener);
         passwordEditText.addTextChangedListener(afterTextChangedListener);
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
+        loginButton.setOnClickListener(v -> {
+            Intent intent = new Intent();
+            intent.setClass(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
         });
     }
 }
