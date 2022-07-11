02ebcf7bb709ce60a4e66ec6e66a02626a6ae4c6
ZuoWu-TD <gsjtceoxl@163.com>
2022-07-11 11:06:16 +0800

    ui update

--- app/src/main/java/com/lmcteam/worldcube/ui/widget/WCubeTextView.java
+++ app/src/main/java/com/lmcteam/worldcube/ui/widget/WCubeTextView.java
@@ -1,19 +1,12 @@
 package com.lmcteam.worldcube.ui.widget;
 
 import android.annotation.SuppressLint;
-import android.os.Handler;
-import android.os.Message;
 
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 import com.lmcteam.worldcube.util.AssetsUtil;
 
 public class WCubeTextView extends androidx.appcompat.widget.AppCompatTextView {
-
-    private String mOriginalStr;
-    private long mDuration = 500;
-    private int mIndex = 0;
-    private Handler mHandler;
-    private final int SHOW_NEXT_CHAR = 1;
-
 
     public WCubeTextView(android.content.Context context) {
         super(context);
@@ -34,42 +27,13 @@ public class WCubeTextView extends androidx.appcompat.widget.AppCompatTextView {
     @SuppressLint("HandlerLeak")
     public void init() {
         setTypeface(AssetsUtil.getTypeface());
-        mOriginalStr = getText().toString();
-        this.setText("");
 
-        mHandler = new Handler() {
-            @SuppressLint({"HandlerLeak", "SetTextI18n"})
-            public void handleMessage(Message msg) {
-                if (msg.what == SHOW_NEXT_CHAR && mIndex < mOriginalStr.length()) {
-                    WCubeTextView.this.setText(WCubeTextView.this.getText().toString()
-                            + mOriginalStr.charAt(mIndex));
-                    mIndex++;
-                }
-
-            }
-        };
-        start();
     }
 
-    public void setDuration(long duration) {
-        mDuration = duration;
+    @Override
+    public void setText(CharSequence text, BufferType type) {
+        super.setText(text, type);
+        YoYo.with(Techniques.Landing).duration(700).playOn(this);
     }
-
-    private void start() {
-        new Thread() {
-            public void run() {
-                while (mIndex < mOriginalStr.length()) {
-                    try {
-                        Thread.sleep(mDuration);
-                        mHandler.sendEmptyMessage(SHOW_NEXT_CHAR);
-                    } catch (Exception ex) {
-                        ex.printStackTrace();
-                    }
-
-                }
-            }
-        }.start();
-    }
-
 }
 
